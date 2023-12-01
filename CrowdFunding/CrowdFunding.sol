ba43ba535533fb4c781b39d73ebee27fe52712eb
Nil-yt <101439701+NiL-yt@users.noreply.github.com>
2023-12-01 19:41:17 +0800

    Delete CrowdFunding directory

--- CrowdFunding/CrowdFunding.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.6.12 <0.9.0; //版本指令
-contract crowdfuding{
-    address public beneficiary; //受益人
-
-    mapping(address => uint) public funders; //捐赠人地址到捐赠金额的映射
-}
